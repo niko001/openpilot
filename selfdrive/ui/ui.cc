afb0f3059de693e6b4f4ef68c529db4441e202af
Nikolaus Fischer <nikolaus_fischer@t-online.de>
2024-12-17 15:59:03 +0100

    fixed engagement button check logic

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -235,10 +235,10 @@ void UIState::updateStatus() {
     }
 
     // Get the events that caused the state transition
-    const auto car_events = controls_state.getEvents();
+    const auto events = controls_state.getCarEvents();
     bool button_enable = false;
-    for (const auto& event : car_events) {
-      if (event.name == cereal::CarEvent::EventName::buttonEnable) {
+    for (const auto& event : events) {
+      if (event.name() == cereal::CarEvent::EventName::BUTTON_ENABLE) {
         button_enable = true;
         break;
       }
