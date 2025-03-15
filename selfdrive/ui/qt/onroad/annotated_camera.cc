8c98da2f891f2b87827011884b20b69025a0da56
Nikolaus Fischer <nikolaus_fischer@t-online.de>
2025-03-15 18:40:02 +0100

    fixed syntax error in annotated_camera.cc

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -167,7 +167,7 @@ void AnnotatedCameraWidget::paintGL() {
   double fps = fps_filter.update(1. / dt * 1000);
   if (fps < 15) {
     //LOGW("slow frame rate: %.2f fps", fps);
-  }i
+  }
   prev_draw_t = cur_draw_t;
 
   // publish debug msg
