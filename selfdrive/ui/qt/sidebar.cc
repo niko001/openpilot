ba0a685529845f662f52f46e37e5053419a9de8a
Nikolaus Fischer <nikolaus_fischer@t-online.de>
2025-03-15 15:39:53 +0100

    added params declaration in sidebar.css once more

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -115,6 +115,7 @@ void Sidebar::updateState(const UIState &s) {
 }
 
 void Sidebar::paintEvent(QPaintEvent *event) {
+  Params params;
   QPainter p(this);
   p.setPen(Qt::NoPen);
   p.setRenderHint(QPainter::Antialiasing);
