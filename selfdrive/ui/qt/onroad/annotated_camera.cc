7d5b056a9fcad020ab136e863dafd2d96ded4de7
Nikolaus Fischer <nikolaus_fischer@t-online.de>
2025-03-15 18:20:31 +0100

    suppress slow frame rate logging

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -166,7 +166,7 @@ void AnnotatedCameraWidget::paintGL() {
   double dt = cur_draw_t - prev_draw_t;
   double fps = fps_filter.update(1. / dt * 1000);
   if (fps < 15) {
-    LOGW("slow frame rate: %.2f fps", fps);
+    //LOGW("slow frame rate: %.2f fps", fps);
   }
   prev_draw_t = cur_draw_t;
 
