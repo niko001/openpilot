4778cd71b2269ab6803315c0e0cb466dfc6019c4
Nikolaus Fischer <nikolaus_fischer@t-online.de>
2025-03-18 15:57:26 +0100

    debugging

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -151,25 +151,6 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
                                            "../assets/offroad/icon_road.png",
                                            distance_options);
 
-  // Set the current value
-  std::string current_distance = params.get("WazeAlertsDistance");
-  if (current_distance.empty()) {
-    // Default to 200m if not set
-    params.put("WazeAlertsDistance", "200");
-    distance_btn->setCheckedButton(default_distance_idx);
-  } else {
-    // Find the index of the current distance value
-    auto it = std::find(distance_values.begin(), distance_values.end(), QString::fromStdString(current_distance));
-    if (it != distance_values.end()) {
-      int idx = std::distance(distance_values.begin(), it);
-      distance_btn->setCheckedButton(idx);
-    } else {
-      // If not found, default to 200m
-      distance_btn->setCheckedButton(default_distance_idx);
-    }
-  }
-
-  // No need to connect - ButtonParamControl handles the parameter update internally
 
   addItem(distance_btn);
   distance_btn->setEnabled(params.getBool("WazeAlertsEnabled"));
