63cb901b842d24e6bcb080b9612ca7e8358b820e
Nikolaus Fischer <nikolaus_fischer@t-online.de>
2025-03-18 13:06:57 +0100

    fix clicked 2

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -169,12 +169,7 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
     }
   }
 
-  // Connect button value change to update the distance
-  QObject::connect(distance_btn, &ButtonParamControl::buttonClicked, [=](int index) {
-    if (index >= 0 && index < distance_values.size()) {
-      params.put("WazeAlertsDistance", distance_values[index].toStdString());
-    }
-  });
+  // No need to connect - ButtonParamControl handles the parameter update internally
 
   addItem(distance_btn);
   distance_btn->setEnabled(params.getBool("WazeAlertsEnabled"));
