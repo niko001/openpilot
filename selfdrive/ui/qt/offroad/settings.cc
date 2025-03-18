d6c4eaed50cb64d9717b73cfff58229469162f48
Nikolaus Fischer <nikolaus_fischer@t-online.de>
2025-03-18 16:26:34 +0100

    debugging 11

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -208,7 +208,7 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
   distanceLayout->addWidget(increaseBtn);
 
   // Logic for button clicks
-  QObject::connect(decreaseBtn, &QPushButton::clicked, [&p, valueLabel, =]() {
+  QObject::connect(decreaseBtn, &QPushButton::clicked, [=, &p]() {
     int current_val = 0;
     try {
       std::string val_str = p.get("WazeAlertsDistance");
@@ -229,7 +229,7 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
     }
   });
 
-  QObject::connect(increaseBtn, &QPushButton::clicked, [&p, valueLabel, =]() {
+  QObject::connect(increaseBtn, &QPushButton::clicked, [=, &p]() {
     int current_val = 0;
     try {
       std::string val_str = p.get("WazeAlertsDistance");
